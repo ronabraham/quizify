cf25ebe4415f56dcf8192ab6a0dd5d656f8083d5
Ron Abraham <ron.abraham@rajyaseva.in>
2026-07-23 11:51:23 +0530

    Initial Commit

---
+++ quizify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  quizify
+//
+//  Created by Ron Abraham on 23/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ quizify/quizifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizifyApp.swift
+//  quizify
+//
+//  Created by Ron Abraham on 23/07/26.
+//
+
+import SwiftUI
+
+@main
+struct quizifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
